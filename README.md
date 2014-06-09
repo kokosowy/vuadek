f6b70a3214318b5d172d02ab95e32ddfa4552882
Kokos <konrad.kostecki@gmail.com>
2014-06-09 12:55:53 +0200

    Updated some brief information in README and added path support from command line.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ vuadek
 ======
 
 vUADEk
+
+This is a self-learning project in matter of Python programming and usage of git. I needed batch tool which will help me with mass convartion of Amiga music modules to mp3s. I hope the project will grow and grow.. :-)
+
+Work In Progress.
+
+REQUIREMENTS:
+
+- Linux
+- Python (testing in 3.4)
+- UADE
+- lame
+
+
--- vuadek.py
+++ vuadek.py
@@ -1,14 +1,9 @@
-print("vUADEk")
+#!/usr/bin/python3.4
 
-
+import sys
 import os
 import subprocess
 
-#mport json
-
-zm_input = "spacedeb.suk"
-zm_output = "test.wav"
-zm_mp3 = "test.mp3"
 zm_home = os.path.expanduser("~")
 zm_pth_workdir = zm_home+"/.vuadek/"
 
@@ -16,8 +11,9 @@ if not os.path.exists(zm_pth_workdir):
     os.makedirs(zm_pth_workdir)
 zm_fl_remains = zm_pth_workdir+"remains"
 
+pathname = os.path.dirname(sys.argv[1])
 
-scan_result = subprocess.Popen(["uade123", "--scan", zm_home+"/Dokumenty"],stdout=subprocess.PIPE)
+scan_result = subprocess.Popen(["uade123", "--scan", os.path.abspath(pathname)],stdout=subprocess.PIPE)
 
 with open(zm_fl_remains, 'w') as f:
 	for line in scan_result.stdout:
@@ -27,8 +23,10 @@ f.closed
 with open(zm_fl_remains, 'r') as f:
 	zm_input = [line.rstrip('\n') for line in f]
 	for item in zm_input:
-		subprocess.call(["uade123", "--detect-format-by-content", "-f",zm_output, "--filter=A1200", "--normalise", "--speed-hack", "-v", "--headphones", item])
-		print(item)
+		head, tail = os.path.split(item)
+		subprocess.call(["uade123", "--detect-format-by-content", "-f",zm_pth_workdir+tail+'.wav', "--filter=A1200", "--normalise", "--speed-hack", "-v", "--headphones", item])
+		subprocess.call(["lame", "--verbose", "--preset", "standard", zm_pth_workdir+tail+'.wav', head+'/'+tail+'.mp3'])
+		subprocess.call(["rm", zm_pth_workdir+tail+'.wav'])
 f.closed
 
 
